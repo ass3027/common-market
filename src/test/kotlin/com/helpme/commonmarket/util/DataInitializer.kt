638f340d6206b2534216ebe1529a0adab72a3303
ass3027 <blackfeather227@gmail.com>
2025-07-17 14:53:11 +0900

    test(Product-Data): test code for fake data and add datafaker dependency needed for fake data generate

---
+++ src/test/kotlin/com/helpme/commonmarket/util/DataInitializer.kt
@@ -0,0 +1,45 @@
+package com.helpme.commonmarket.util
+
+import com.helpme.commonmarket.product.entity.Product
+import com.helpme.commonmarket.product.repository.ProductRepository
+import net.datafaker.Faker
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class DataInitializer {
+
+    @Autowired
+    private lateinit var productRepository: ProductRepository
+
+    private val faker = Faker()
+
+    @Test
+    fun generateDummyProducts() {
+        val products = mutableListOf<Product>()
+        (1..5_000_000).forEach { i ->
+            products.add(createDummyProduct())
+            if (products.size == 1000) {
+                productRepository.saveAll(products)
+                products.clear()
+                println("insert product $i")
+            }
+        }
+        if (products.isNotEmpty()) {
+            productRepository.saveAll(products)
+        }
+    }
+
+    private fun createDummyProduct(): Product {
+        return Product(
+            name = faker.commerce().productName(),
+            price = faker.number().numberBetween(1000, 1000000).toLong(),
+            sellerId = faker.number().numberBetween(1, 100).toLong(),
+            imageUrl = faker.internet().image(),
+            content = faker.lorem().sentence()
+        )
+    }
+}
