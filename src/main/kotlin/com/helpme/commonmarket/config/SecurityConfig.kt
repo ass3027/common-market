68bf63c1ffa4e2bdb8c909d07034e52ab4bbb6c2
ass3027 <blackfeather227@gmail.com>
2025-07-29 19:57:40 +0900

    refactor(Security): config

--- src/main/kotlin/com/helpme/commonmarket/config/SecurityConfig.kt
+++ src/main/kotlin/com/helpme/commonmarket/config/SecurityConfig.kt
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
@@ -19,8 +18,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 class SecurityConfig(
-    private val customUserDetailsService: CustomUserDetailsService,
-    private val passwordEncoder: PasswordEncoder,
     private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
     private val jwtAuthenticationFilter: JwtAuthenticationFilter
 ) {
@@ -45,26 +42,19 @@ class SecurityConfig(
                     // Any other request needs authentication
                     .anyRequest().authenticated()
             }
-            .authenticationProvider(authenticationProvider())
             .exceptionHandling { it.authenticationEntryPoint(jwtAuthenticationEntryPoint) }
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
     }
 
     @Bean
-    fun authenticationProvider(): DaoAuthenticationProvider {
-        val authProvider = DaoAuthenticationProvider(passwordEncoder)
-        authProvider.setUserDetailsService(customUserDetailsService)
-        return authProvider
-    }
-
-    @Bean
     fun authenticationManager(authConfig: AuthenticationConfiguration): AuthenticationManager {
+        //TODO 이해하기
         return authConfig.authenticationManager
     }
 
     @Bean
-    fun userDetailsService(): UserDetailsService {
-        return customUserDetailsService
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 }
